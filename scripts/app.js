c6dccba010b17077ec8a8c79ead661ad312d661d
Armin Abbasi <apmnh.armin@gmail.com>
2025-06-15 17:19:14 +0000

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -112,3 +112,66 @@ particlesJS('particles-js',
     , function () {
         console.log('callback - particles.js config loaded');
     });
+
+
+const textArray = ["Researcher", "Computer Scientist", "Programmer"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1000;
+let textIndex = 0;
+let charIndex = 0;
+
+const typedTextSpan = document.getElementById("typewriter");
+
+function type() {
+    if (charIndex < textArray[textIndex].length) {
+        typedTextSpan.textContent += textArray[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textIndex = (textIndex + 1) % textArray.length;
+        setTimeout(type, typingDelay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(type, newTextDelay);
+});
+
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function () {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+})();
+
+// Loader
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+
+    setTimeout(() => {
+        loader.classList.add('fade-out');
+    }, 3000);
+});
+
+setTimeout(() => {
+    const loader = document.getElementById('loader');
+    if (loader && !loader.classList.contains('fade-out')) {
+        loader.classList.add('fade-out');
+    }
+}, 5000);
